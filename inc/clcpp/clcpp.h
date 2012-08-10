b312d8997ce5d46c6510d823d6b36690b368657a
dwilliamson <devnull@localhost>
2012-08-10 15:35:51 +0100

    Assert on failure to get correct type name hash or type pointer.

--- inc/clcpp/clcpp.h
+++ inc/clcpp/clcpp.h
@@ -938,6 +938,7 @@ namespace clcpp
 	CLCPP_NOINLINE unsigned int GetTypeNameHash()
 	{
 		static unsigned int hash = CLCPP_INVALID_HASH;
+		internal::Assert(hash != CLCPP_INVALID_HASH);
 		return hash;
 	}
 
@@ -945,6 +946,7 @@ namespace clcpp
 	CLCPP_NOINLINE const Type* GetType()
 	{
 		static const Type* type_ptr = (Type*)CLCPP_INVALID_ADDRESS;
+		internal::Assert(type_ptr != (Type*)CLCPP_INVALID_ADDRESS);
 		return type_ptr;
 	}
 }
