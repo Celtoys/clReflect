b7025b1199588b79f93a0f74a58cd9dd5e130374
sungJinKangdev <hour30000@gmail.com>
2021-11-10 15:19:18 +0900

    fix bugs when stream_set->indent_depth is out of range

--- src/clReflectCore/Logging.cpp
+++ src/clReflectCore/Logging.cpp
@@ -237,6 +237,7 @@ logging::StreamHandle logging::GetStreamHandle(const char* name)
 	return &g_StreamMap[name];
 }
 
+#define CL_MIN(a,b) (((a)<(b))?(a):(b))
 
 void logging::Log(StreamHandle handle, Tag tag, bool do_prefix, const char* format, ...)
 {
@@ -259,11 +260,11 @@ void logging::Log(StreamHandle handle, Tag tag, bool do_prefix, const char* form
 		if (tag == TAG_INFO)
 		{
 			// Kick the prefix off with indent characters
-			for (int i = 0; i < stream_set->indent_depth; i++)
+			for (int i = 0; i < CL_MIN(stream_set->indent_depth, 127); i++)
 			{
 				prefix[i] = '\t';
 			}
-			prefix[stream_set->indent_depth] = 0;
+			prefix[CL_MIN(stream_set->indent_depth, 127)] = 0;
 		}
 
 		// Add any tag annotations
